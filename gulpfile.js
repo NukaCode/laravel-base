8f76fc2601ec716a700cfc516fb2411f18dddaad
Stygian <stygian.warlock.v2@gmail.com>
2015-06-10 08:38:39 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,49 +14,49 @@ var bower_dir = 'vendor/bower_components/';
  */
 
 elixir(function (mix) {
-    mix.less('app.less', 'resources/css/')
+    mix.less('app.less', 'resources/assets/css/')
 
         // Java script
-        .copy(bower_dir + 'jquery/dist/jquery.min.js', 'resources/js/vendor/jquery.js')
-        .copy(bower_dir + 'jquery-ui/jquery-ui.min.js', 'resources/js/vendor/jquery-ui.js')
-        .copy(bower_dir + 'bootstrap/dist/js/bootstrap.min.js', 'resources/js/vendor/bootstrap.js')
-        .copy(bower_dir + 'messenger/build/js/messenger.min.js', 'resources/js/vendor/messenger.js')
-        .copy(bower_dir + 'messenger/build/js/messenger-theme-future.js', 'resources/js/vendor/messenger-theme-future.js')
-        .copy(bower_dir + 'bootbox/bootbox.js', 'resources/js/vendor/bootbox.js')
-        .copy(bower_dir + 'select2/select2.min.js', 'resources/js/vendor/select2.js')
-        .copy(bower_dir + 'metisMenu/dist/metisMenu.min.js', 'resources/js/vendor/metisMenu.js')
-        .copy(bower_dir + 'mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js', 'resources/js/vendor/bootstrap-colorpicker.js')
-        .copy(bower_dir + 'jasny-bootstrap/dist/js/jasny-bootstrap.min.js', 'resources/js/vendor/jasny-bootstrap.js')
+        .copy(bower_dir + 'jquery/dist/jquery.min.js', 'resources/assets/js/vendor/jquery.js')
+        .copy(bower_dir + 'jquery-ui/jquery-ui.min.js', 'resources/assets/js/vendor/jquery-ui.js')
+        .copy(bower_dir + 'bootstrap/dist/js/bootstrap.min.js', 'resources/assets/js/vendor/bootstrap.js')
+        .copy(bower_dir + 'messenger/build/js/messenger.min.js', 'resources/assets/js/vendor/messenger.js')
+        .copy(bower_dir + 'messenger/build/js/messenger-theme-future.js', 'resources/assets/js/vendor/messenger-theme-future.js')
+        .copy(bower_dir + 'bootbox/bootbox.js', 'resources/assets/js/vendor/bootbox.js')
+        .copy(bower_dir + 'select2/select2.min.js', 'resources/assets/js/vendor/select2.js')
+        .copy(bower_dir + 'metisMenu/dist/metisMenu.js', 'resources/assets/js/vendor/metisMenu.js')
+        .copy(bower_dir + 'mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js', 'resources/assets/js/vendor/bootstrap-colorpicker.js')
+        .copy(bower_dir + 'jasny-bootstrap/dist/js/jasny-bootstrap.min.js', 'resources/assets/js/vendor/jasny-bootstrap.js')
         .scripts(
         [
-            'resources/js/vendor/jquery.js',
-            'resources/js/vendor/bootstrap.js',
-            'resources/js/vendor/messenger.js',
-            'resources/js/vendor/messenger-theme-future.js',
-            'resources/js/vendor/bootbox.js',
-            'resources/js/vendor/select2.js',
+            'vendor/jquery.js',
+            'vendor/bootstrap.js',
+            'vendor/messenger.js',
+            'vendor/messenger-theme-future.js',
+            'vendor/bootbox.js',
+            'vendor/select2.js',
         ], 'public/js/all.js')
         .scripts(
         [
-            'resources/js/vendor/jquery.js',
-            'resources/js/vendor/bootstrap.js',
-            'resources/js/vendor/messenger.js',
-            'resources/js/vendor/messenger-theme-future.js',
-            'resources/js/vendor/bootbox.js',
-            'resources/js/vendor/select2.js',
-            'resources/js/vendor/metisMenu.js'
+            'vendor/jquery.js',
+            'vendor/bootstrap.js',
+            'vendor/messenger.js',
+            'vendor/messenger-theme-future.js',
+            'vendor/bootbox.js',
+            'vendor/select2.js',
+            'vendor/metisMenu.js'
         ], 'public/js/admin-all.js')
 
         // CSS
-        .copy(bower_dir + 'font-awesome/css/font-awesome.min.css', 'resources/css/vendor/font-awesome.css')
-        .copy(bower_dir + 'messenger/build/css/messenger.css', 'resources/css/vendor/messenger.css')
-        .copy(bower_dir + 'messenger/build/css/messenger-theme-future.css', 'resources/css/vendor/messenger-theme-future.css')
-        .copy(bower_dir + 'select2/select2.css', 'resources/css/vendor/select2.css')
-        .copy(bower_dir + 'select2-bootstrap3-css/select2-bootstrap.css', 'resources/css/vendor/select2-bootstrap.css')
-        .copy(bower_dir + 'jasny-bootstrap/dist/css/jasny-bootstrap.min.css', 'resources/css/vendor/jasny-bootstrap.min.css')
-        .copy(bower_dir + 'mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css', 'resources/css/vendor/colorpicker/css/bootstrap-colorpicker.min.css')
-        .copy(bower_dir + 'metisMenu/dist/css/metisMenu.css', 'resources/css/vendor/metisMenu.css')
-        .copy(bower_dir + 'nukacode-admin/css/admin.css', 'resources/css/vendor/admin.css')
+        .copy(bower_dir + 'font-awesome/css/font-awesome.min.css', 'resources/assets/css/vendor/font-awesome.css')
+        .copy(bower_dir + 'messenger/build/css/messenger.css', 'resources/assets/css/vendor/messenger.css')
+        .copy(bower_dir + 'messenger/build/css/messenger-theme-future.css', 'resources/assets/css/vendor/messenger-theme-future.css')
+        .copy(bower_dir + 'select2/select2.css', 'resources/assets/css/vendor/select2.css')
+        .copy(bower_dir + 'select2-bootstrap3-css/select2-bootstrap.css', 'resources/assets/css/vendor/select2-bootstrap.css')
+        .copy(bower_dir + 'jasny-bootstrap/dist/css/jasny-bootstrap.min.css', 'resources/assets/css/vendor/jasny-bootstrap.min.css')
+        .copy(bower_dir + 'mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css', 'resources/assets/css/vendor/colorpicker/css/bootstrap-colorpicker.min.css')
+        .copy(bower_dir + 'metisMenu/dist/metisMenu.css', 'resources/assets/css/vendor/metisMenu.css')
+        .copy(bower_dir + 'nukacode-admin/css/admin.css', 'resources/assets/css/vendor/admin.css')
         .styles(
         [
             'app.css',
@@ -68,13 +68,13 @@ elixir(function (mix) {
         ], 'public/css/all.css')
         .styles(
         [
-            'resources/css/vendor/admin.css',
-            'resources/css/vendor/font-awesome.css',
-            'resources/css/vendor/messenger.css',
-            'resources/css/vendor/messenger-theme-future.css',
-            'resources/css/vendor/select2.css',
-            'resources/css/vendor/select2-bootstrap.css',
-            'resources/css/vendor/metisMenu.css'
+            'vendor/admin.css',
+            'vendor/font-awesome.css',
+            'vendor/messenger.css',
+            'vendor/messenger-theme-future.css',
+            'vendor/select2.css',
+            'vendor/select2-bootstrap.css',
+            'vendor/metisMenu.css'
         ], 'public/css/admin-all.css')
 
         // Extras
